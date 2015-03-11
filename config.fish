c78c495e93244f5c7b3956b73f4144ace5962fa6
Joseph Martinot-Lagarde <joseph.martinot-lagarde@onera.fr>
2015-03-11 09:45:17 +0100

    Ajout de dossiers courants au PATH

--- config.fish
+++ config.fish
@@ -1,5 +1,11 @@
 set --erase fish_greeting
 
+for dir in /usr/local/anaconda/bin ~/bin ~/.bin
+    if [ -d $dir ]
+        set PATH $dir $PATH
+    end
+end
+
 # start X at login
 #if status --is-login
 #    if test -z "$DISPLAY" -a $XDG_VTNR = 1
